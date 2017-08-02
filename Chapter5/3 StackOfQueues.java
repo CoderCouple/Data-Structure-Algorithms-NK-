687f3f2e5ce22af720227a550258cc06eef01601
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-02 11:21:12 -0700

    Update 3 StackOfQueues.java

--- Chapter5/3 StackOfQueues.java
+++ Chapter5/3 StackOfQueues.java
@@ -1,13 +1,85 @@
 package Chapter5;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class StackOfQueues {
 
+	Queue<Integer> q1;
+	Queue<Integer> q2;
+
 	public StackOfQueues() {
-		// TODO Auto-generated constructor stub
+		q1 = new LinkedList<Integer>();
+		q2 = new LinkedList<Integer>();
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void push(int x) {
+		q1.add(x);
+	}
+
+	public void shuffleQueues(Queue<Integer> qu1, Queue<Integer> qu2) {
+		while (!qu1.isEmpty()) {
+			qu2.add(qu1.remove());
+		}
+	}
+
+	public int pop() throws Exception {
+		if (q1.isEmpty())
+			throw new Exception("Queue is Empty...!!!");
+		shuffleQueues(q1, q2);
+		int x = q2.remove();
+		shuffleQueues(q2, q1);
+
+		return x;
+	}
+
+	public int peek() throws Exception {
+		if (q1.isEmpty())
+			throw new Exception("Queue is Empty...!!!");
+
+		shuffleQueues(q1, q2);
+		int x = q2.peek();
+		shuffleQueues(q2, q1);
+
+		return x;
+	}
+
+	public int size() {
+		return q1.size();
+	}
+
+	public boolean isEmpty() {
+		return q1.isEmpty();
+	}
+
+	public void printit() {
+		if (q1.isEmpty())
+			return;
+		int x = q1.remove();
+		System.out.print(x);
+		System.out.print(" ");
+		printit();
+		q1.add(x);
+	}
+	public static void main(String[] args) throws Exception {
+		StackOfQueues soq = new StackOfQueues();
+		soq.push(1);
+		soq.push(2);
+		soq.push(3);
+		soq.printit();
+		System.out.println();
+		System.out.println("===========================");
+		System.out.println(soq.pop());
+		soq.printit();
+		System.out.println();
+		System.out.println("===========================");
+		soq.push(4);
+		soq.printit();
+		System.out.println();
+		System.out.println("===========================");
+		System.out.println(soq.peek());
+		System.out.println(soq.size());
+		System.out.println(soq.isEmpty());
 
 	}
 
