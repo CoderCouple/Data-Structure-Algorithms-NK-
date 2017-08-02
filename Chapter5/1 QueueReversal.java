b12616fe920edf0bd31c8fafb0f4bb75371ea761
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-01 19:36:15 -0700

    Create 1 QueueReversal.java

---
+++ Chapter5/1 QueueReversal.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class QueueReversal {
+
+	public QueueReversal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
