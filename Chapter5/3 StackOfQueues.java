63483db93b870a6d0033ace872967a5543990c66
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-01 20:55:52 -0700

    Create 3 StackOfQueues.java

---
+++ Chapter5/3 StackOfQueues.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class StackOfQueues {
+
+	public StackOfQueues() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
