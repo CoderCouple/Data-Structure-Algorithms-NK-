5e76070ff4f17c3bd1a3d6eec544da7a3b320a59
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-01 19:46:42 -0700

    Update 1 QueueReversal.java

--- Chapter5/1 QueueReversal.java
+++ Chapter5/1 QueueReversal.java
@@ -1,13 +1,38 @@
 package Chapter5;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class QueueReversal {
 
-	public QueueReversal() {
-		// TODO Auto-generated constructor stub
+	public static void getReversedQueue(Queue<Integer> q) {
+		if (q.isEmpty())
+			return;
+
+		Stack<Integer> s = new Stack<Integer>();
+
+		while (!q.isEmpty()) {
+			s.push(q.remove());
+		}
+
+		while (!s.isEmpty()) {
+			q.add(s.pop());
+		}
+
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		q.add(5);
+
+		System.out.println(q);
+		getReversedQueue(q);
+		System.out.println(q);
 
 	}
 
