7f2ab0e61632c278b7344d8e405c102f23d2d4dc
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-03 11:46:30 -0700

    Update 4 MaxOfTheWindow.java

--- Chapter5/4 MaxOfTheWindow.java
+++ Chapter5/4 MaxOfTheWindow.java
@@ -15,26 +15,28 @@ public class MaxOfTheWindow {
 		return max;
 
 	}
-	
-	public static void getMaxWindow(int array [],int windowLength){
-		if(array.length<=0 || array.length-windowLength<0)
+
+	public static void getMaxWindow(int array[], int windowLength) {
+		if (array.length <= 0 || array.length - windowLength < 0)
 			return;
-		
-		int max=0;
-		int wn=0;
-		for(int i=0;i<array.length-windowLength;i++){
-			int temp=getMaxOfTheWindow(array,i,windowLength);
-			if(temp>max){
-				max=temp;
-				wn=i;
+
+		int max = 0;
+		int wn = 0;
+		for (int i = 0; i <= array.length - windowLength; i++) {
+			int temp = getMaxOfTheWindow(array, i, windowLength);
+			if (temp > max) {
+				max = temp;
+				wn = i;
 			}
 		}
-		
-		System.out.println("Window Number : "+wn);
+
+		System.out.println("Window Number : " + wn);
+		System.out.println("Maximum element : " + max);
+
 	}
 
 	public static void main(String[] args) {
-		int array []= {1,3,-1,-3,5,3,6,7};
+		int array[] = { 1, 3, -1, -3, 5, 3, 6, 7 };
 		getMaxWindow(array, 3);
 	}
 
